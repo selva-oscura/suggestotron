f6d81a7e458b0cde5442908f8841c6d7050789f0
selva-oscura <selva_oscura@yahoo.com>
2013-07-13 14:46:16 -0700

    changed gemfile for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Suggestotron::Application.routes.draw do
+  resources :topics
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,6 +52,7 @@ Suggestotron::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+root :to => 'topics#index'
 
   # See how all your routes lay out with "rake routes"
 
